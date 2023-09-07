0d187522b2cdf797b2c283edd0296c1736f60817
David Kang <3746349+hahagu@users.noreply.github.com>
2023-09-07 14:39:11 +0800

    Update preview icon

--- Widget.cs
+++ Widget.cs
@@ -2,17 +2,22 @@
 using System;
 using System.Drawing;
 using WigiDashWidgetFramework.WidgetUtility;
+using System.IO;
+using PictureWidget;
 
 namespace HotkeyWidget {
     public partial class HotkeyWidget : IWidgetObject {
 
         // Functionality
         public string ResourcePath;
+        private Bitmap icon;
+
         public WidgetError Load(string resource_path) {
             
             this.ResourcePath = resource_path;
 
             // Load previews
+            icon = new Bitmap(Path.Combine(ResourcePath, "icon.png"));
 
             return WidgetError.NO_ERROR;
         }
@@ -34,11 +39,10 @@ namespace HotkeyWidget {
             Color BackColor = Color.FromArgb(35, 35, 35);
             Size size = widget_size.ToSize();
             Bitmap BitmapPreview = new Bitmap(size.Width, size.Height);
+
             using(Graphics g = Graphics.FromImage(BitmapPreview)) {
                 g.Clear(BackColor);
-                Font FontHeader = new Font("Lucida Console", 20, FontStyle.Bold);
-                SizeF str_size = g.MeasureString("Hotkey", FontHeader);
-                g.DrawString("Hotkey", FontHeader, Brushes.White, (size.Width - str_size.Width) / 2, (size.Height - str_size.Height) / 2);
+                g.DrawImageZoomedToFit(icon, size.Width, size.Height);
             }
             return BitmapPreview;
         }
